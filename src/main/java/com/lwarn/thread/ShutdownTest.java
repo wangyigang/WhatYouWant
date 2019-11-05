f958e432fe9141045f3b9e36a8306e848aa8c43e
wangyigang <1010234991@qq.com>
2019-11-05 21:49:53 +0800

    第一次提交：

---
+++ src/main/java/com/lwarn/thread/ShutdownTest.java
@@ -0,0 +1,15 @@
+package com.lwarn.thread;
+
+public class ShutdownTest {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static  class Runner implements  Runnable{
+        //重写run方法
+        public void run() {
+
+        }
+    }
+}
